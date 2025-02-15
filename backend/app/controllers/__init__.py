d516a16e1c54f7d0705e11fc466897c937f89335
Pierre AHOBLE <ahosipik@gmail.com>
2025-02-15 13:30:09 +0000

    authentication

--- backend/app/controllers/__init__.py
+++ backend/app/controllers/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from app.controllers.authentication_controller import authentication_bp
 from app.controllers.agent_controller import agent_bp, create as createAgent, index as indexAgent
 from app.controllers.user_controller import user_bp, create as createUser, login_superviseur
 
 from app.extensions import docs
 
 def register_blueprints(app: Flask):
+   app.register_blueprint(authentication_bp, url_prefix='/api/auth')
    app.register_blueprint(agent_bp, url_prefix='/api/agent')
    # Home controller route login_superviseur
    app.register_blueprint(user_bp, url_prefix="/api/user")
